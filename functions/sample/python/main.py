66a6e9de40f60a89b081029d33e09ac305c6d3c2
fkanedev <136233923+fkanedev@users.noreply.github.com>
2024-02-13 20:38:39 +0000

    Update main.py

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -35,59 +35,57 @@ def main(param_dict):
     return {"dbs": client.all_dbs()}
 
 
+#def main(param_dict):
+#    """Main Function
+#
+#    Args:
+#        param_dict (Dict): input paramater
+#
+#    Returns:
+#        _type_: _description_ TODO
+#    """
+#
+#    try:
+#        client = Cloudant.iam(
+#            account_name=param_dict["COUCH_USERNAME"],
+#            api_key=param_dict["IAM_API_KEY"],
+#            connect=True,
+#        )
+#        #print(f"Databases: {client.all_dbs()}")
+#        reviews_db = client['reviews']
+#        selector = {'_id': {'$eq': param_dict["dealerId"]}}
+#        docs = reviews_db.get_query_result(selector)
 
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        #print(f"Databases: {client.all_dbs()}")
-        reviews_db = client['reviews']
-        selector = {'_id': {'$eq': param_dict["dealerId"]}}
-        docs = reviews_db.get_query_result(selector)
-
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"docs": docs}
-
+#    except CloudantException as cloudant_exception:
+#        print("unable to connect")
+#        return {"error": cloudant_exception}
+#    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+#        print("connection error")
+#        return {"error": err}
+#
+#   return {"docs": docs}
 
 
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        #print(f"Databases: {client.all_dbs()}")
-        reviews_db = client['reviews']
-        selector = {'_id': {'$eq': param_dict["dealerId"]}}
-        docs = reviews_db.get_query_result(selector)
+#def main(param_dict):
+#    """Main Function
+#
+#    Args:
+#        param_dict (Dict): input paramater
+#
+#    Returns:
+#        _type_: _description_ TODO
+#    """
+#
+#    try:
+#        client = Cloudant.iam(
+#            account_name=param_dict["COUCH_USERNAME"],
+#            api_key=param_dict["IAM_API_KEY"],
+#            connect=True,
+#        )
+#        #print(f"Databases: {client.all_dbs()}")
+#        reviews_db = client['reviews']
+#        selector = {'_id': {'$eq': param_dict["dealerId"]}}
+#        docs = reviews_db.get_query_result(selector)
 
     except CloudantException as cloudant_exception:
         print("unable to connect")
